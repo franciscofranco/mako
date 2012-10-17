464dc2f2ee55ea329256e9ab2773ee18658a3fba
Hemant Kumar <hemantk@codeaurora.org>
2012-10-17 12:29:51 -0700

    EHCI: HSIC: Allow standalone power collapse when HSIC is active
    HSIC generates interrupt at 4ms interval. If idle power collapse
    is enabled when HSIC is active, krait keeps on going into idle
    power collapse requesting RPM to handle the sleep set requirement.
    If HSIC interrupt comes after 4ms interval, krait comes out of idle
    power collapse and it again ask RPM to handle this request therefore
    keeps RPM busy handling requests due to frequent idle power collapse.
    This has a power impact. Hence only allow standalone power collapse
    when HSIC is active to avoid RPM involvement in assisting kraits to
    power collapse. Vote for idle power collapse when HSIC goes to low
    power mode.
    
    Change-Id: Iee8bc1fa180499f2d26fbac955a6a6ed767fdeb4
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -2915,6 +2915,7 @@ static void enable_avc_i2c_bus(void)
 static void __init apq8064_common_init(void)
 {
 	u32 platform_version;
+	struct msm_rpmrs_level rpmrs_level;
 	platform_device_register(&msm_gpio_device);
 	msm_tsens_early_init(&apq_tsens_pdata);
 	msm_thermal_init(&msm_thermal_pdata);
@@ -2949,8 +2950,12 @@ static void __init apq8064_common_init(void)
 		platform_add_devices(common_not_mpq_devices,
 			ARRAY_SIZE(common_not_mpq_devices));
 	enable_ddr3_regulator();
-	msm_hsic_pdata.swfi_latency =
-		msm_rpmrs_levels[0].latency_us;
+	rpmrs_level =
+		msm_rpmrs_levels[MSM_PM_SLEEP_MODE_WAIT_FOR_INTERRUPT];
+	msm_hsic_pdata.swfi_latency = rpmrs_level.latency_us;
+	rpmrs_level =
+		msm_rpmrs_levels[MSM_PM_SLEEP_MODE_POWER_COLLAPSE_STANDALONE];
+	msm_hsic_pdata.standalone_latency = rpmrs_level.latency_us;
 	if (machine_is_apq8064_mtp()) {
 		msm_hsic_pdata.log2_irq_thresh = 5,
 		apq8064_device_hsic_host.dev.platform_data = &msm_hsic_pdata;
