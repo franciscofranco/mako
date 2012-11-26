739640b0b5d1cea551f21e18ea96a75d3440f935
franciscofranco <franciscofranco.1990@gmail.com>
2012-11-25 22:19:08 -0800

    arch: arm: mako: increase threshold of the thermal control.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1391,7 +1391,7 @@ static struct platform_device msm_tsens_device = {
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 7,
 	.poll_ms = 250,
-	.limit_temp_degC = 60,
+	.limit_temp_degC = 80,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 };
