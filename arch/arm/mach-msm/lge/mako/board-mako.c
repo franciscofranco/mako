986979acf584596028717d797c222f8530890f9e
franciscofranco <franciscofranco.1990@gmail.com>
2012-12-15 21:52:40 -0800

    mach-msm: mako: according to @morfic we can change the sensor id to 0 and stop thermald binary and the kernel will manage the heat by itself from its drivers. So lets test it.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1389,8 +1389,8 @@ static struct platform_device msm_tsens_device = {
 };
 
 static struct msm_thermal_data msm_thermal_pdata = {
-	.sensor_id = 7,
-	.poll_ms = 250,
+	.sensor_id = 0,
+	.poll_ms = 500,
 	.limit_temp_degC = 80,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
