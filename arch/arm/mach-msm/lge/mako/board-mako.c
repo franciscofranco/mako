0a50f17cf4e3839a42ae6503c23f6d7d6fb83141
franciscofranco <franciscofranco.1990@gmail.com>
2012-12-20 00:00:16 -0800

    board: mako: decrease the previous throttle temperature from 80ºC to 70ºC. Now that we're not using proprietary Qualcomm Thermald anymore msm_thermal works fine.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1391,7 +1391,7 @@ static struct platform_device msm_tsens_device = {
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 500,
-	.limit_temp_degC = 80,
+	.limit_temp_degC = 70,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 };
