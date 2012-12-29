c96ae5871af76c5b3530643516fd89d5f1b1019f
Harsh Bhanvadia <harsh.slade@gmail.com>
2012-12-29 10:39:03 +0100

    mako: hsic: Disallow processor idle sleep while driving resume signal
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -2023,6 +2023,8 @@ static void __init apq8064_common_init(void)
 	platform_add_devices(common_devices, ARRAY_SIZE(common_devices));
 	platform_add_devices(common_not_mpq_devices,
 			ARRAY_SIZE(common_not_mpq_devices));
+	msm_hsic_pdata.swfi_latency =
+		msm_rpmrs_levels[0].latency_us;
 	apq8064_device_hsic_host.dev.platform_data = &msm_hsic_pdata;
 	device_initialize(&apq8064_device_hsic_host.dev);
 	apq8064_pm8xxx_gpio_mpp_init();
