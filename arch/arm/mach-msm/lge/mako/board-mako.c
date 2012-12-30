30abf17de652ce726ece4f796313a97138e62cef
franciscofranco <franciscofranco.1990@gmail.com>
2012-12-30 01:20:47 -0800

    board: mako: tune thermal control to be a little bit more aggressive, this should help decreasing the device temperature on heavy loads.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1391,8 +1391,8 @@ static struct platform_device msm_tsens_device = {
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 500,
-	.limit_temp_degC = 70,
-	.temp_hysteresis_degC = 10,
+	.limit_temp_degC = 60,
+	.temp_hysteresis_degC = 5,
 	.freq_step = 2,
 };
 
