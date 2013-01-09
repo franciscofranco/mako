806018761c13265b294effd6ed9bcecca49b6170
franciscofranco <franciscofranco.1990@gmail.com>
2013-01-09 14:22:44 -0800

    auto_hotplug.c: we're now scaling the cores based on the NVIDIA's avg_running code so now the driver will be less hard on the system because we got rid of the history cycle and multiplication before deciding when to online/offline cores.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -77,7 +77,7 @@ unsigned char flags;
  */
 
 static unsigned int enable_all_load_threshold __read_mostly = 600;
-static unsigned int enable_load_threshold __read_mostly = 250;
+static unsigned int enable_load_threshold __read_mostly = 275;
 static unsigned int disable_load_threshold __read_mostly = 125;
 
 module_param(enable_all_load_threshold, int, 0775);
@@ -92,13 +92,10 @@ struct delayed_work hotplug_offline_work;
 struct work_struct hotplug_offline_all_work;
 struct work_struct hotplug_boost_online_work;
 
-static unsigned int history[SAMPLING_PERIODS];
-static unsigned int index;
-
 static void hotplug_decision_work_fn(struct work_struct *work)
 {
-	unsigned int running, disable_load, sampling_rate, enable_load, avg_running = 0;
-	unsigned int online_cpus, available_cpus, i, j;
+	unsigned int disable_load, sampling_rate, enable_load, avg_running = 0;
+	unsigned int online_cpus, available_cpus;
 #if DEBUG
 	unsigned int k;
 #endif
@@ -107,14 +104,6 @@ static void hotplug_decision_work_fn(struct work_struct *work)
 	available_cpus = CPUS_AVAILABLE;
 	disable_load = disable_load_threshold * online_cpus;
 	enable_load = enable_load_threshold * online_cpus;
-	
-	/*
-	 * Multiply nr_running() by 100 so we don't have to
-	 * use fp division to get the average.
-	 */
-	running = nr_running() * 100;
-
-	history[index] = running;
 
 #if DEBUG
 	pr_info("online_cpus is: %d\n", online_cpus);
@@ -123,25 +112,6 @@ static void hotplug_decision_work_fn(struct work_struct *work)
 	pr_info("index is: %d\n", index);
 	pr_info("running is: %d\n", running);
 #endif
-
-	/*
-	 * Use a circular buffer to calculate the average load
-	 * over the sampling periods.
-	 * This will absorb load spikes of short duration where
-	 * we don't want additional cores to be onlined because
-	 * the cpufreq driver should take care of those load spikes.
-	 */
-	for (i = 0, j = index; i < SAMPLING_PERIODS; i++, j--) {
-		avg_running += history[j];
-		if (unlikely(j == 0))
-			j = INDEX_MAX_VALUE;
-	}
-
-	/*
-	 * If we are at the end of the buffer, return to the beginning.
-	 */
-	if (unlikely(index++ == INDEX_MAX_VALUE))
-		index = 0;
 
 #if DEBUG
 	pr_info("array contents: ");
@@ -152,7 +122,7 @@ static void hotplug_decision_work_fn(struct work_struct *work)
 	pr_info("avg_running before division: %d\n", avg_running);
 #endif
 
-	avg_running = avg_running / SAMPLING_PERIODS;
+	avg_running = avg_nr_running() / SAMPLING_PERIODS;
 
 #if DEBUG
 	pr_info("average_running is: %d\n", avg_running);
