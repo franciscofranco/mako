38179891eb20afd712b5cd274f6d5b3d1dd5fe4a
motley <motley.slate@gmail.com>
2013-01-19 16:06:22 -0500

    net: usb: Abort suspend if get_encap response is not sent
    After receiving response available notification driver queues
    a work to send get_encap response. pm state is set to
    RPM_SUSPENDING before interface suspend is initiated. If
    interface suspend happens after queuing the work then work
    will be executed and blocked until it is done. Work tries to
    resume the interface synchronously by calling rpm_resume().
    This causes rpm_resume() to wait indefinitely for the pm state
    to change from RPM_SUSPENDED to RPM_RESUME which will not
    happen until interface suspend returns EBUSY. Hence avoid this
    situation by aborting interface suspend if work is pending
    or running.
    
    CRs-Fixed: 415208
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Change-Id: Ib2a079a7107d994f760c0a876736ede3add07ea2Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/net/usb/rmnet_usb_ctrl.c
+++ drivers/net/usb/rmnet_usb_ctrl.c
@@ -339,9 +339,11 @@ int rmnet_usb_ctrl_start_rx(struct rmnet_ctrl_dev *dev)
 
 int rmnet_usb_ctrl_suspend(struct rmnet_ctrl_dev *dev)
 {
-	if (!flush_work_sync(&dev->get_encap_work))
-		usb_kill_anchored_urbs(&dev->rx_submitted);
+	if (work_busy(&dev->get_encap_work))
+			return -EBUSY;
 
+	usb_kill_anchored_urbs(&dev->rx_submitted);
+	
 	return 0;
 }
 static int rmnet_usb_ctrl_alloc_rx(struct rmnet_ctrl_dev *dev)
