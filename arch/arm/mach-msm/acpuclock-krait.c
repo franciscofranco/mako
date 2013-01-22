b1ff00d7136d764a4e6d750ee5326aeadddccd56
morfic <morfic@gmail.com>
2013-01-22 00:08:51 -0600

    Mako (absolute) Voltage control.
    This started out as just including the existing work of someone else.
    But that was so obfuscated and needlessly moved in sysfs, i turned it into the type control
    we have on NP, meaning same sysfs location and output formatting.
    Since the various kernels range from no preUV to different levels of UV it is best to encourage the user
    to think in absolute voltages.
    "I can UV -150 on kernel X! Really? Odd, i can only UV kernel Y -50mV!"
    Kernel X has no preUV, kernel Y has 100mV preUV, so they both run the same.
    This is needlessly confusing to users.
    
    Talking in absolutes, users will get a better understanding of "what the other guy's" device is capable of.
    Sticking to a common sysfs location and to the common absolute voltage is thus the right thing to do.
    
    This seems to work now as it should.
    
    This is heavily based on coolbho3k's NP patch. from Paul's mess we might have a few function names, but not the contents.
    This is not his, if he claims otherwise, he can go fuck himself.
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -825,6 +825,58 @@ static void __init bus_init(const struct l2_level *l2_level)
 		dev_err(drv.dev, "initial bandwidth req failed (%d)\n", ret);
 }
 
+#ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
+
+#define USERCONTROL_MIN_VDD		 750
+#define USERCONTROL_MAX_VDD		1300
+#define NUM_FREQS			18
+
+ssize_t acpuclk_get_vdd_levels_str(char *buf) {
+
+	int i, len = 0;
+
+	if (buf) {
+		for (i = 0; drv.acpu_freq_tbl[i].speed.khz; i++) {
+			if (drv.acpu_freq_tbl[i].use_for_scaling) {
+				len += sprintf(buf + len, "%lumhz: %i mV\n", drv.acpu_freq_tbl[i].speed.khz/1000,
+						drv.acpu_freq_tbl[i].vdd_core/1000 );
+			}
+		}
+	}
+	return len;
+}
+
+ssize_t acpuclk_set_vdd(char *buf) {
+
+        int i = 0;
+        unsigned long volt_cur[NUM_FREQS] = {0};
+        int ret = 0;
+
+	if (buf) {
+		ret = sscanf(buf, "%lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu",
+				&volt_cur[0], &volt_cur[1], &volt_cur[2], &volt_cur[3], &volt_cur[4], &volt_cur[5], &volt_cur[6], &volt_cur[7], &volt_cur[8],
+				&volt_cur[9], &volt_cur[10], &volt_cur[11], &volt_cur[12], &volt_cur[13], &volt_cur[14], &volt_cur[15], &volt_cur[16], &volt_cur[17]);
+
+		if (ret != NUM_FREQS)
+			return -EINVAL;
+
+		for(i = 0; i < NUM_FREQS; i++) {
+			if(drv.acpu_freq_tbl[i].speed.khz != 0) {
+
+				if (volt_cur[i] < (unsigned long) USERCONTROL_MIN_VDD)
+					volt_cur[i] = (unsigned long) USERCONTROL_MIN_VDD;
+                                if (volt_cur[i] > (unsigned long) USERCONTROL_MAX_VDD)
+                                        volt_cur[i] = (unsigned long) USERCONTROL_MAX_VDD;
+
+				drv.acpu_freq_tbl[i].vdd_core = volt_cur[i]*1000;
+
+			}
+		}
+	}
+	return ret;
+}
+#endif
+
 #ifdef CONFIG_CPU_FREQ_MSM
 static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
 
