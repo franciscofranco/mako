c2c116c677026b5c1579906b34c65656ca43071c
franciscofranco <franciscofranco.1990@gmail.com>
2013-02-25 03:22:45 -0800

    Revert "drivers: otg: add USB fast_charge."
    This reverts commit e920a46a3bbe856b69ec3ea3d2f1e3973c2162ca.

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -51,9 +51,6 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
-bool fast_charge = false;
-module_param(fast_charge, bool, 0775);
-
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1111,9 +1108,6 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 	if (motg->cur_power == mA)
 		return;
 
-	if (fast_charge)
-		mA = 1000;
-	
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 
 	pm8921_charger_vbus_draw(mA);
