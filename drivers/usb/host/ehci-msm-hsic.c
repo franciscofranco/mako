54025173c391ba4a1281ab9547f779b6a84effc5
Francisco Franco <franciscofranco.1990@gmail.com>
2013-03-19 01:49:07 -0700

    hsic: decrease autosuspend a little bit.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -1346,9 +1346,9 @@ static int ehci_hsic_bus_resume(struct usb_hcd *hcd)
 static void ehci_msm_set_autosuspend_delay(struct usb_device *dev)
 {
 	if (!dev->parent) /*for root hub no delay*/
-		pm_runtime_set_autosuspend_delay(&dev->dev, 160);
+		pm_runtime_set_autosuspend_delay(&dev->dev, 100);
 	else
-		pm_runtime_set_autosuspend_delay(&dev->dev, 800);
+		pm_runtime_set_autosuspend_delay(&dev->dev, 400);
 }
 
 static struct hc_driver msm_hsic_driver = {
