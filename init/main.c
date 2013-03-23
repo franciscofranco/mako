6763e351e287205bee9156cc0bec2ec958d8824c
franciscofranco <franciscofranco.1990@gmail.com>
2013-03-23 14:35:44 -0700

    init: main: commented out ftrace_init() because I don't use ftrace for released kernels, only for my own debugging. Should save few ms during boot.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- init/main.c
+++ init/main.c
@@ -193,13 +193,13 @@ EXPORT_SYMBOL(loops_per_jiffy);
 
 static int __init debug_kernel(char *str)
 {
-	console_loglevel = 10;
+	console_loglevel = 0;
 	return 0;
 }
 
 static int __init quiet_kernel(char *str)
 {
-	console_loglevel = 4;
+	console_loglevel = 0;
 	return 0;
 }
 
@@ -635,7 +635,7 @@ asmlinkage void __init start_kernel(void)
 	acpi_early_init(); /* before LAPIC and SMP init */
 	sfi_init_late();
 
-	ftrace_init();
+	//ftrace_init();
 
 	/* Do the rest non-__init'ed, we're now alive */
 	rest_init();
