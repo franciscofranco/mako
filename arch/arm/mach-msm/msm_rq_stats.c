58c9c18327b7663d92e3fcc7b22a158e51ff7c5c
Francisco Franco <franciscofranco.1990@gmail.com>
2013-04-14 02:48:21 -0700

    rq_stats: don't need to call the update_average_load every POSTCHANGE transition, specially because we're already calling update_average_load every msecs_to_jiffies(HZ) from the Hotplug driver. With all the locks & unlocks involved plus all the time calling and making some calculations (divisions as well) we may as well don't have the stats driver to call it that much.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -181,7 +181,6 @@ static int cpufreq_transition_handler(struct notifier_block *nb,
 		for_each_cpu(j, this_cpu->related_cpus) {
 			struct cpu_load_data *pcpu = &per_cpu(cpuload, j);
 			mutex_lock(&pcpu->cpu_load_mutex);
-			update_average_load(freqs->old, freqs->cpu);
 			pcpu->cur_freq = freqs->new;
 			mutex_unlock(&pcpu->cpu_load_mutex);
 		}
