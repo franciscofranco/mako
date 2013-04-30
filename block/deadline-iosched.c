157c8c8a29d7c1140702ba73809ed2b3a98a9b84
Francisco Franco <franciscofranco.1990@gmail.com>
2013-04-29 17:26:36 -0700

    block: deadline: fix magic values.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -17,8 +17,8 @@
 /*
  * See Documentation/block/deadline-iosched.txt
  */
-static const int read_expire = 250;  /* max time before a read is submitted. */
-static const int write_expire = 2500; /* ditto for writes, these limits are SOFT! */
+static const int read_expire = 25;  /* max time before a read is submitted. */
+static const int write_expire = 250; /* ditto for writes, these limits are SOFT! */
 static const int writes_starved = 1;    /* max times reads can starve a write */
 static const int fifo_batch = 8;       /* # of sequential requests treated as one
 				     				by the above parameters. For throughput. */
