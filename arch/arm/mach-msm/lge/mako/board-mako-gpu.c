dc2d64c7b8513c69dc392911736cf5ff63dd0dd7
Francisco Franco <franciscofranco.1990@gmail.com>
2013-05-13 04:28:25 +0100

    mako: gpu: lets give him extra MBPS(bust speed) on the lowest freq.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/lge/mako/board-mako-gpu.c
+++ arch/arm/mach-msm/lge/mako/board-mako-gpu.c
@@ -69,13 +69,13 @@ static struct msm_bus_vectors grp3d_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1000),
+		.ib = KGSL_CONVERT_TO_MBPS(1500),
 	},
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D_PORT1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1000),
+		.ib = KGSL_CONVERT_TO_MBPS(1500),
 	},
 };
 
