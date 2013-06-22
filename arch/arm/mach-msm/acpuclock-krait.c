bb7cdf3e9882c86d73380c6807012dfa0ef253d5
Francisco Franco <franciscofranco.1990@gmail.com>
2013-06-22 17:45:40 +0100

    msm: krait: export CPU type to userspace instead of having it buried on the kmsg log so that app developers can show it on their apps.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -22,6 +22,7 @@
 #include <linux/cpufreq.h>
 #include <linux/cpu.h>
 #include <linux/regulator/consumer.h>
+#include <linux/module.h> 
 
 #include <asm/mach-types.h>
 #include <asm/cpu.h>
@@ -48,6 +49,9 @@
 
 static DEFINE_MUTEX(driver_lock);
 static DEFINE_SPINLOCK(l2_lock);
+
+char *cpu_type = "UNKNOWN";
+module_param(cpu_type, charp, 0755);
 
 static struct drv_data {
 	struct acpu_level *acpu_freq_tbl;
@@ -1130,7 +1134,17 @@ static int __init get_pvs_bin(u32 pte_efuse)
 		pvs_bin = 0;
 		dev_warn(drv.dev, "ACPU PVS: Defaulting to %d\n", pvs_bin);
 	} else {
-		dev_info(drv.dev, "ACPU PVS: %d\n", pvs_bin);
+        if (pvs_bin == 1)
+            cpu_type = "SLOW";
+        else if (pvs_bin == 2)
+            cpu_type = "NOMINAL";
+        else if (pvs_bin == 3)
+            cpu_type = "FAST";
+        else if (pvs_bin == 4)
+            cpu_type = "FASTEST";
+        
+		dev_info(drv.dev, "ACPU PVS: %s\n", cpu_type);
+
 	}
 
 	return pvs_bin;
