db6167cd7574b095d8c4aa2e787a4630d4126694
Francisco Franco <franciscofranco.1990@gmail.com>
2013-08-27 22:45:47 +0100

    block: cfq: merge tweaked tunables.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -21,18 +21,18 @@
  * tunables
  */
 /* max queue in one round of service */
-static const int cfq_quantum = 8;
-static const int cfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
+static const int cfq_quantum = 4;
+static const int cfq_fifo_expire[2] = {42, 11};
 /* maximum backwards seek, in KiB */
-static const int cfq_back_max = 16 * 1024;
+static const int cfq_back_max = 12582912;
 /* penalty of a backwards seek */
-static const int cfq_back_penalty = 2;
-static const int cfq_slice_sync = HZ / 10;
-static int cfq_slice_async = HZ / 25;
+static const int cfq_back_penalty = 1;
+static const int cfq_slice_sync = HZ / 8;
+static int cfq_slice_async = 7;
 static const int cfq_slice_async_rq = 2;
-static int cfq_slice_idle = HZ / 125;
-static int cfq_group_idle = HZ / 125;
-static const int cfq_target_latency = HZ * 3/10; /* 300 ms */
+static int cfq_slice_idle = 0;
+static int cfq_group_idle = 0;
+static const int cfq_target_latency = 300; /* 300 ms */
 static const int cfq_hist_divisor = 4;
 
 /*
