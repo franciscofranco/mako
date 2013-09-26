12f3c8150758b44ff0a0a7dc5f2c54dc58bbab00
Francisco Franco <franciscofranco.1990@gmail.com>
2013-09-26 06:31:51 +0100

    drivers: input: major improvements on the doubletap to wake feature.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -57,8 +57,8 @@ u64 freq_boosted_time;
 
 bool suspended = false;
 
-bool touch_to_wake = false;
-module_param(touch_to_wake, bool, 0664);
+bool doubletap_to_wake = false;
+module_param(doubletap_to_wake, bool, 0664);
 
 #define LGE_TOUCH_ATTR(_name, _mode, _show, _store)               \
 	struct lge_touch_attribute lge_touch_attr_##_name =       \
@@ -806,7 +806,8 @@ static void touch_input_report(struct lge_touch_data *ts)
 
 struct double_tap_to_wake {
 	unsigned long touch_time;
-	unsigned long continuous_touch_time;
+	unsigned long window_time;
+	unsigned long sample_time_ms;
 	unsigned int touches;
 	struct input_dev *input_device;
 };
@@ -836,33 +837,23 @@ static void touch_work_func(struct work_struct *work)
 		freq_boosted_time = ktime_to_ms(ktime_get());
 	}
 
-	if (suspended && touch_to_wake)
+	if (suspended && doubletap_to_wake)
 	{
-		if (wake.touches == 0)
-			wake.continuous_touch_time = ktime_to_ms(ktime_get());
-
-		if (wake.touch_time == 0)
-			wake.touch_time = ktime_to_ms(ktime_get());
-		
-		if (wake.touch_time > 0)
+		if (!(wake.touch_time + 2000 >= ktime_to_ms(ktime_get())))
 		{
-			if (wake.touch_time + 2000 >= ktime_to_ms(ktime_get()))
-			{
-				if (wake.touches < 2)
-				{
-					if (wake.continuous_touch_time + (wake.touches * 100) <= ktime_to_ms(ktime_get()))
-					{
-						pr_info("Touches: %d\n", wake.touches);
-						wake.touches++;
-					}
-				}
-			}
-			else
-			{
-				wake.touch_time = 0;
-				wake.touches = 0;	
-			}
+			wake.touch_time = ktime_to_ms(ktime_get());
+			wake.touches = 0;
+		}
 			
+		if (!time_is_after_jiffies(
+			wake.window_time + msecs_to_jiffies(wake.sample_time_ms)))
+		{
+			/*
+			 * Don't count as touch when we release the touch input
+			 */
+			if (ts->ts_data.curr_data[0].state != ABS_RELEASE)
+				++wake.touches;
+
 			if (wake.touches == 2)
 			{
 				input_event(wake.input_device, EV_KEY, KEY_POWER, 1);
@@ -870,8 +861,12 @@ static void touch_work_func(struct work_struct *work)
 				msleep(100);
 				input_event(wake.input_device, EV_KEY, KEY_POWER, 0);
 				input_event(wake.input_device, EV_SYN, 0, 0);
-			}						
+
+				input_sync(wake.input_device);	
+			}
 		}
+
+		wake.window_time = jiffies;
 	} 
 
 	atomic_dec(&ts->next_work);
@@ -1790,7 +1785,8 @@ static int touch_probe(struct i2c_client *client,
 	int one_sec = 0;
 
 	wake.touch_time = 0;
-	wake.continuous_touch_time = 0;
+	wake.window_time = jiffies;
+	wake.sample_time_ms = 100;
 	wake.touches = 0;
 
 	if (unlikely(touch_debug_mask & DEBUG_TRACE))
@@ -2083,7 +2079,7 @@ static void touch_early_suspend(struct early_suspend *h)
 		return;
 	}
 
-	if (touch_to_wake)
+	if (doubletap_to_wake)
 	{
 		enable_irq_wake(ts->client->irq);
 	}
@@ -2120,7 +2116,7 @@ static void touch_late_resume(struct early_suspend *h)
 		return;
 	}
 
-	if (touch_to_wake)
+	if (doubletap_to_wake)
 	{
 		disable_irq_wake(ts->client->irq);
 	}
@@ -2195,7 +2191,7 @@ int touch_driver_register(struct touch_device_driver* driver)
 
 	touch_device_func = driver;
 
-	touch_wq = create_singlethread_workqueue("touch_wq");
+	touch_wq = alloc_workqueue("touch_wq", 0, 1);
 	if (!touch_wq) {
 		TOUCH_ERR_MSG("CANNOT create new workqueue\n");
 		ret = -ENOMEM;
