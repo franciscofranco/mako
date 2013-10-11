b3cf9aa88dd0e696dfb7f13b187d38a127c10b5e
Francisco Franco <franciscofranco.1990@gmail.com>
2013-10-11 18:52:50 +0100

    cpufreq: interactive: 1GHz is too low because it uses a lower bus frequency, thus some lags are noticeable. Using 1134MHz is notoriously better due to the usage of the max bus frequency. Power usage should be bigger, but I'd rather have the device smoother.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -120,7 +120,7 @@ static bool io_is_busy = true;
  * The CPU will be boosted to this frequency when the screen is
  * touched. input_boost needs to be enabled.
  */
-#define DEFAULT_INPUT_BOOST_FREQ 1026000
+#define DEFAULT_INPUT_BOOST_FREQ 1134000
 static int input_boost_freq = DEFAULT_INPUT_BOOST_FREQ;
 
 bool interactive_selected = true;
