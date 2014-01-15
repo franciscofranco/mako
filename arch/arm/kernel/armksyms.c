e7e7e33075f7b42ac62a87873a652991b7e546ab
franciscofranco <franciscofranco.1990@gmail.com>
2014-01-15 02:39:08 +0000

    Revert bulk of string patches.

--- arch/arm/kernel/armksyms.c
+++ arch/arm/kernel/armksyms.c
@@ -83,6 +83,10 @@ EXPORT_SYMBOL(memset);
 EXPORT_SYMBOL(memchr);
 EXPORT_SYMBOL(__memzero);
 
+	/* user mem (segment) */
+EXPORT_SYMBOL(__strnlen_user);
+EXPORT_SYMBOL(__strncpy_from_user);
+
 #ifdef CONFIG_MMU
 EXPORT_SYMBOL(copy_page);
 
