1944d076c1a9cd92089853a4bbfb85cab5f5eae6
Dennis Rassmann <showp1984@gmail.com>
2014-04-09 11:55:39 +0200

    lge_touch_core: i2c_read: retry a few times before erroring out
    Patch from Dennis Rassmann (showp1984) -- I've reduced the number of
    retries to 4.
    
    Initial commit message:
    
    --
    The i2c devices we are connecting to may not be ready yet, or just
    busy. Retry with a 10ms pause in order to still obtain touch data.
    This improves overall touch data robustness, but the main thing it
    does:
    WE CAN FINALLY GET TOUCH DATA IN SUSPEND ALL THE TIME.
    --
    
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: Mikael Berthe <mikael@lilotux.net>

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -145,6 +145,8 @@ void* get_touch_handle(struct i2c_client *client)
  */
 int touch_i2c_read(struct i2c_client *client, u8 reg, int len, u8 *buf)
 {
+#define LGETOUCH_I2C_RETRY 3
+	int retry = 0;
 	struct i2c_msg msgs[] = {
 		{
 			.addr = client->addr,
@@ -160,13 +162,18 @@ int touch_i2c_read(struct i2c_client *client, u8 reg, int len, u8 *buf)
 		},
 	};
 
-	if (i2c_transfer(client->adapter, msgs, 2) < 0) {
-		if (printk_ratelimit())
-			TOUCH_ERR_MSG("transfer error\n");
-		return -EIO;
-	} else {
-		return 0;
+	for (retry = 0; retry <= LGETOUCH_I2C_RETRY; retry++) {
+		if (i2c_transfer(client->adapter, msgs, 2) == 2)
+			break;
+		if (retry == LGETOUCH_I2C_RETRY) {
+			if (printk_ratelimit())
+				TOUCH_ERR_MSG("transfer error\n");
+			return -EIO;
+		} else
+			msleep(10);
 	}
+
+	return 0;
 }
 
 int touch_i2c_write(struct i2c_client *client, u8 reg, int len, u8 * buf)
