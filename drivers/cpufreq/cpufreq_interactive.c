aaa3d7b1ddb07a7b625446969e35a7cc884190c8
franciscofranco <franciscofranco.1990@gmail.com>
2014-06-30 04:41:05 +0000

    cpufreq: interactive: this governor is fundamentally different from Ondemand and by norm its a bit more sensitive to load changes in terms of which freqs are chosen, but even though sometimes it can be a little stupid and non react to load changes that linearly as one could hope for (even though it's better than Ondemand) so for that reason let's try to use this new formula by @stratosk found in this commit: https://github.com/stratosk/kernel_msm/commit/9df03b662b9c213c75ac082138f5ab8afadb9d94 make sure you read his commit message for better understanding of this.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -322,6 +322,15 @@ static unsigned int choose_freq(
 	return freq;
 }
 
+static unsigned int calc_freq(struct cpufreq_interactive_cpuinfo *pcpu, 
+	unsigned int load)
+{
+	unsigned int max = pcpu->policy->cpuinfo.max_freq;
+	unsigned int min = pcpu->policy->cpuinfo.min_freq;
+
+	return min + load * (max - min) / 100;
+}
+
 static u64 update_load(int cpu)
 {
 	struct cpufreq_interactive_cpuinfo *pcpu = &per_cpu(cpuinfo, cpu);
@@ -394,15 +403,14 @@ static void cpufreq_interactive_timer(unsigned long data)
 			new_freq = hispeed_freq;
 		else
 		{
-			new_freq = choose_freq(pcpu, loadadjfreq);
-
+			new_freq = calc_freq(pcpu, cpu_load);
 			if (new_freq < hispeed_freq)
 				new_freq = hispeed_freq;
 		}
 	}
 	else
 	{
-		new_freq = choose_freq(pcpu, loadadjfreq);
+		new_freq = calc_freq(pcpu, cpu_load);
 		if (new_freq > hispeed_freq &&
 					new_freq < pcpu->target_freq)
 				new_freq = hispeed_freq;
@@ -449,7 +457,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	pcpu->hispeed_validate_time = now;
 
 	if (cpufreq_frequency_table_target(pcpu->policy, pcpu->freq_table,
-					   new_freq, CPUFREQ_RELATION_L,
+					   new_freq, CPUFREQ_RELATION_C,
 					   &index)) {
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto rearm;
