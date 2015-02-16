370da595bb3100746c2665f71bbe6e04b4d95d11
franciscofranco <franciscofranco.1990@gmail.com>
2015-02-16 04:31:44 +0000

    wakeup: add userspace knob to enable/disable msm_hsic_host wakelock
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -14,10 +14,14 @@
 #include <linux/suspend.h>
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
+#include <linux/moduleparam.h>
 
 #include "power.h"
 
 #define TIMEOUT		100
+
+static bool enable_msm_hsic_ws = true;
+module_param(enable_msm_hsic_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -374,6 +378,11 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
  */
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
+	if (!enable_msm_hsic_ws && !strcmp(ws->name, "msm_hsic_host")) {
+		pr_info("wakeup source msm_hsic_host activate skipped\n");
+		return;
+	}
+
 	ws->active = true;
 	ws->active_count++;
 	ws->last_time = ktime_get();
